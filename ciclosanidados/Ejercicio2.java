c776eed19f386a39c8c75518818501b114b566e1
Juan SP <juan-220597@hotmail.com>
2024-09-11 15:22:12 -0600

    Se agrego la carpeta de ciclosanidados, en donde vienen los archivos de java, de los ejercicios que se realizaron este dia

---
+++ ciclosanidados/Ejercicio2.java
@@ -0,0 +1,12 @@
+package ciclosanidados;
+
+public class Ejercicio2 {
+	 public static void main(String[] args) {
+		 for (int i = 6; i >= 1; i--) {
+	            for (int j = 1; j <= i; j++) {
+	                System.out.print("* ");
+	            }
+	            System.out.println();  // Para saltar a la siguiente línea después de cada fila
+	        }
+	 }
+}
---
+++ ciclosanidados/Ejercicio3.java
@@ -0,0 +1,12 @@
+package ciclosanidados;
+
+public class Ejercicio3 {
+	  public static void main(String[] args) {
+	        for (int i = 1; i <= 6; i++) {
+	            for (int j = 1; j <= i; j++) {
+	                System.out.print("* ");
+	            }
+	            System.out.println(); // Para saltar a la siguiente línea después de cada fila
+	        }
+	    }
+}
